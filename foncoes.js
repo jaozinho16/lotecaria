3ae7ae8dc90db756f773c586a7be26bb39b3a31b
jaozinho16 <souzamatosojoaolucas@gmail.com>
2022-02-22 18:39:33 -0300

    arrumando o index

---
+++ foncoes.js
@@ -0,0 +1,16 @@
+var numSort = []
+function sorteioNum(){
+    do{
+        let sort = Math.ceil(Math.random() * 60)
+
+        console.long(sorte)
+        numSort.push(sorte)
+
+    } white(numSort.length <6 )
+   
+}
+
+
+
+
+
